a85c71a8f8600a828a2b386053f1edcdaf60d7c1
Christian David Becerra Montoya <68264172+cdavidbm@users.noreply.github.com>
2025-10-21 15:25:35 -0500

    Update grid dimensions for the game

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #pgzero
 
 celda = 50
-anchura_en_cantidad_de_celdas = 5
-altura_en_cantidad_de_celdas = 5
+anchura_en_cantidad_de_celdas = 7
+altura_en_cantidad_de_celdas = 7
 WIDTH = celda * anchura_en_cantidad_de_celdas
 HEIGHT = celda * altura_en_cantidad_de_celdas
 TITLE = "Mazmorras"
