419b3e90d6b4da9b948c0de92c1702a9d02af5eb
Christian David Becerra Montoya <68264172+cdavidbm@users.noreply.github.com>
2025-10-21 15:41:17 -0500

    Rename map variable and add custom drawing function

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ HEIGHT = celda * altura_en_cantidad_de_celdas
 TITLE = "Mazmorras"
 FPS = 30
 
-my_map = [[0, 0, 0, 0, 0, 0, 0], 
+mi_mapa = [[0, 0, 0, 0, 0, 0, 0], 
           [0, 1, 2, 1, 3, 1, 0], 
           [0, 1, 1, 2, 1, 1, 0], 
           [0, 3, 2, 1, 1, 3, 0], 
@@ -16,14 +16,13 @@ my_map = [[0, 0, 0, 0, 0, 0, 0],
           [0, 1, 3, 1, 1, 2, 0], 
           [0, 0, 0, 0, 0, 0, 0]]
 
-
-def map_draw():
-    for i in range(len(my_map)):
-        for j in range(len(my_map[0])):
-            cell.left = cell.width*j
-            cell.top = cell.height*i
-            cell.draw()
+def dibujar_mi_propio_mapa():
+    for i in range(len(dibujar_mi_propio_mapa)):
+        for j in range(len(dibujar_mi_propio_mapa[0])):
+            celda.left = celda.width*j
+            celda.top = celda.height*i
+            celda.draw()
 
 def draw():
-    map_draw()
-    
+    dibujar_mi_propio_mapa()
+
