fd76f1d6353e74993b5eac886e69c92366407592
Christian David Becerra Montoya <68264172+cdavidbm@users.noreply.github.com>
2025-10-21 15:31:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #pgzero
 
 celda = 50
-anchura_en_cantidad_de_celdas = 7
-altura_en_cantidad_de_celdas = 7
+anchura_en_cantidad_de_celdas = 5
+altura_en_cantidad_de_celdas = 5
 WIDTH = celda * anchura_en_cantidad_de_celdas
 HEIGHT = celda * altura_en_cantidad_de_celdas
 TITLE = "Mazmorras"
 FPS = 30
 
+my_map = [[0, 0, 0, 0, 0, 0, 0], 
+          [0, 1, 2, 1, 3, 1, 0], 
+          [0, 1, 1, 2, 1, 1, 0], 
+          [0, 3, 2, 1, 1, 3, 0], 
+          [0, 1, 1, 1, 3, 1, 0], 
+          [0, 1, 3, 1, 1, 2, 0], 
+          [0, 0, 0, 0, 0, 0, 0]]
