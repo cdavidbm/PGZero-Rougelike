15b834312f06ea08c3297bef19cf5d56b6cc883f
Christian David Becerra Montoya <68264172+cdavidbm@users.noreply.github.com>
2025-10-21 15:25:05 -0500

    Initialize main.py with game settings

---
+++ main.py
@@ -0,0 +1,10 @@
+#pgzero
+
+celda = 50
+anchura_en_cantidad_de_celdas = 5
+altura_en_cantidad_de_celdas = 5
+WIDTH = celda * anchura_en_cantidad_de_celdas
+HEIGHT = celda * altura_en_cantidad_de_celdas
+TITLE = "Mazmorras"
+FPS = 30
+
