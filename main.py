c3b4c60ef5ea5726cac7e059c62181629f83f5de
Christian David Becerra Montoya <68264172+cdavidbm@users.noreply.github.com>
2025-10-21 15:39:17 -0500

    Add map_draw and draw functions for rendering

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ my_map = [[0, 0, 0, 0, 0, 0, 0],
           [0, 1, 1, 1, 3, 1, 0], 
           [0, 1, 3, 1, 1, 2, 0], 
           [0, 0, 0, 0, 0, 0, 0]]
+
+
+def map_draw():
+    for i in range(len(my_map)):
+        for j in range(len(my_map[0])):
+            cell.left = cell.width*j
+            cell.top = cell.height*i
+            cell.draw()
+
+def draw():
+    map_draw()
+    
