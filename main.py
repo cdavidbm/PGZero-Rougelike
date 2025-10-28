f1718b07423be2a4fdd3d83a503107de4b083af5
Christian David Becerra Montoya <68264172+cdavidbm@users.noreply.github.com>
2025-10-28 15:23:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,76 @@
 #pgzero
 
-celda = Actor("border")
-celda1 = Actor("crack")
-celda2 = Actor("bones")
-celda3 = Actor("floor")
-anchura_en_cantidad_de_celdas = 7
-altura_en_cantidad_de_celdas = 7
-WIDTH = celda.width * anchura_en_cantidad_de_celdas
-HEIGHT = celda.height * altura_en_cantidad_de_celdas
-TITLE = "Mazmorras"
-FPS = 30
+# Ventana de juego
+cell = Actor('border')
+cell1 = Actor('floor')
+cell2 = Actor("crack")
+cell3 = Actor("bones")
+size_w = 9 # Anchura del campo en celdas
+size_h = 10 # Altura del campo en celdas
+WIDTH = cell.width * size_w
+HEIGHT = cell.height * size_h
 
-personaje = Actor("stand")
-personaje.salud = 100
-personaje.ataque = 5
+TITLE = "Mazmorras" # Título de la ventana de juego
+FPS = 30 # Número de fotogramas por segundo
+my_map = [[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 1, 1, 1, 1, 1, 1, 1, 0], 
+          [0, 1, 1, 2, 1, 3, 1, 1, 0], 
+          [0, 1, 1, 1, 2, 1, 1, 1, 0], 
+          [0, 1, 3, 2, 1, 1, 3, 1, 0], 
+          [0, 1, 1, 1, 1, 3, 1, 1, 0], 
+          [0, 1, 1, 3, 1, 1, 2, 1, 0], 
+          [0, 1, 1, 1, 1, 1, 1, 1, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [-1, -1, -1, -1, -1, -1, -1, -1, -1]] # Fila de poder de ataque y salud
 
-mi_mapa = [[0, 0, 0, 0, 0, 0, 0], 
-          [0, 1, 2, 1, 3, 1, 0], 
-          [0, 1, 1, 2, 1, 1, 0], 
-          [0, 3, 2, 1, 1, 3, 0], 
-          [0, 1, 1, 1, 3, 1, 0], 
-          [0, 1, 3, 1, 1, 2, 0], 
-          [0, 0, 0, 0, 0, 0, 0]]
+# Protagonista
+char = Actor('stand')
+char.top = cell.height
+char.left = cell.width
+char.health = 100
+char.attack = 5
 
-def dibujar_mi_propio_mapa():
-    for i in range(len(mi_mapa)):
-        for j in range(len(mi_mapa[0])):
-            if mi_mapa[i][j] == 0:
-                celda.left = celda.width*j
-                celda.top = celda.height*i
-                celda.draw()
-            elif mi_mapa[i][j] == 1:
-                celda1.left = celda1.width*j
-                celda1.top = celda1.height*i
-                celda1.draw()
-            elif mi_mapa[i][j] == 2:
-                celda2.left = celda2.width*j
-                celda2.top = celda2.height*i
-                celda2.draw()
-            elif mi_mapa[i][j] == 3:
-                celda3.left = celda3.width*j
-                celda3.top = celda3.height*i
-                celda3.draw()
+def map_draw():
+    for i in range(len(my_map)):
+        for j in range(len(my_map[0])):
+            if my_map[i][j] == 0:
+                cell.left = cell.width*j
+                cell.top = cell.height*i
+                cell.draw()
+            elif my_map[i][j] == 1:
+                cell1.left = cell.width*j
+                cell1.top = cell.height*i
+                cell1.draw()
+            elif my_map[i][j] == 2:
+                cell2.left = cell.width*j
+                cell2.top = cell.height*i
+                cell2.draw()  
+            elif my_map[i][j] == 3:
+                cell3.left = cell.width*j
+                cell3.top = cell.height*i
+                cell3.draw() 
 
 def draw():
-    dibujar_mi_propio_mapa()
-    personaje.draw()
-    screen.draw.text(personaje.salud, center=(325, 10), color = 'white', fontsize = 16)
-    screen.draw.text(personaje.ataque, center=(325, 25), color = 'white', fontsize = 16)
+    screen.fill("#2f3542")
+    map_draw()
+    char.draw()
+    screen.draw.text("HP:", center=(25, 475), color = 'white', fontsize = 20)
+    screen.draw.text(char.health, center=(75, 475), color = 'white', fontsize = 20)
+    screen.draw.text("AP:", center=(375, 475), color = 'white', fontsize = 20)
+    screen.draw.text(char.attack, center=(425, 475), color = 'white', fontsize = 20)
 
 def on_key_down(key):
-    if keyboard.right and personaje.x + celda.width < WIDTH - celda.width:
-        personaje.x += celda.width
-    elif keyboard.left and personaje.x - celda.width > celda.width:
-        personaje.x -= celda.width
-    elif keyboard.up:
-        personaje.y -= celda.height
-    elif keyboard.down:
-        personaje.y += celda.height
-    
-    
+    if keyboard.right and char.x + cell.width < WIDTH - cell.width:
+        char.x += cell.width
+        char.image = 'stand'
+    elif keyboard.left and char.x - cell.width > cell.width:
+        char.x -= cell.width
+        char.image = 'left'
+    elif keyboard.down and char.y + cell.height < HEIGHT - cell.height*2:
+        char.y += cell.height
+    elif keyboard.up and char.y - cell.height > cell.height:
+        char.y -= cell.height
+
 
 
 """
